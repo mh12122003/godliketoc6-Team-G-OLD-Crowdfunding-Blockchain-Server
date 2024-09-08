f58f271b1a491e989fac61f6ebdd15456a782665
Daniel Nguyen <83233160+godliketoc6@users.noreply.github.com>
2024-09-08 17:24:13 +0700

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,46 +2,38 @@ import "dotenv/config"
 import express, { NextFunction, Request, Response } from "express";
 import userRoutes from "./routes/users";
 import morgan from "morgan";
-import createHttpError, { isHttpError } from "http-errors";
+import createHttpError, {isHttpError} from "http-errors";
 import session from "express-session";
-import env from "./util/validateEnv";
+import env from"./util/validateEnv";
 import MongoStore from "connect-mongo";
 import cors from "cors";
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:5173", "https://seal-app-a7lmw.ondigitalocean.app", "https://gold-t693d.ondigitalocean.app"];
-
 app.use(morgan("dev"));
+
 app.use(express.json());
 
 app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: ["http://localhost:5173", "https://seal-app-a7lmw.ondigitalocean.app"],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(session({
-  secret: env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 60 * 60 * 1000,
-    secure: true, // Use true if your site is HTTPS
-    httpOnly: true,
-    sameSite: 'none' // Important for cross-site cookies
-  },
-  rolling: true,
-  store: MongoStore.create({
-    mongoUrl: env.MONGO_CONNECTION_STRING,
-  }),
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60 * 60 * 1000,
+        sameSite: 'none',
+        secure: true
+    },
+    rolling: true,
+    store: MongoStore.create({
+        mongoUrl: env.MONGO_CONNECTION_STRING,
+    }),
 }));
 
 app.use("/api/users", userRoutes);
@@ -54,11 +46,10 @@ app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
     console.error(error);
     let errorMessage = "An unknown error occurred";
     let statusCode = 500;
-    if (isHttpError(error)) {
+    if(isHttpError(error)) {
         statusCode = error.status;
         errorMessage = error.message;
     }
-    res.status(statusCode).json({ message: errorMessage });
-});
-
+    res.status(statusCode).json({message: errorMessage});
+})
 export default app;
