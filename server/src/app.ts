4d02cea3807fec72fc28e9dba6f83447458a61d9
Daniel Nguyen <83233160+godliketoc6@users.noreply.github.com>
2024-09-08 11:19:37 +0700

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(cors({
     origin: ["http://localhost:5173", "https://seal-app-a7lmw.ondigitalocean.app", "https://gold-t693d.ondigitalocean.app"],
     credentials: true, // This is important for cookies/sessions
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
 
 app.use(session({
